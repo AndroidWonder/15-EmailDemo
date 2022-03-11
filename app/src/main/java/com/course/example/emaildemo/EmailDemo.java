1ce16e73ba82b54ef27b44e3538512635e292dc0
Android Wonder <jim.pepe@verizon.net>
2022-03-11 17:04:58 -0500

    update rev

--- app/src/main/java/com/course/example/emaildemo/EmailDemo.java
+++ app/src/main/java/com/course/example/emaildemo/EmailDemo.java
@@ -29,13 +29,9 @@ public class EmailDemo extends Activity {
             	msg.putExtra(Intent.EXTRA_CC, new String[] {"HarryBentley@bentley.edu", "HenryBentley@gmail.com"});
             	msg.putExtra(Intent.EXTRA_BCC, new String[] {"HarrietBentley@bentley.edu", "MegBentley@gmail.com"});
             	msg.putExtra(Intent.EXTRA_TEXT, "Hope you're still enjoying class.");
-            	msg.putExtra(Intent.EXTRA_SUBJECT, "Email Demo");            	
-        	    
-            	//check to be sure email is installed on handset
-            	if (msg.resolveActivity(getPackageManager()) != null) {
-        	        startActivity(msg);
-        	    }
-        	    
+            	msg.putExtra(Intent.EXTRA_SUBJECT, "Email Demo");
+
+				startActivity(msg);
         		
         	}
         });
